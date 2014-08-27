a30441001a6c5f163e64fdc2228ee488c4cd05ac
DennisFantoni <dennis@fantoni.dk>
2014-08-27 22:52:54 +0200

    runners

--- robot05.sh
+++ robot05.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 PYTHONPATH=${PYTHONPATH}:/home/ubuntu01/IdeaProjects/QuantoniOanda/oanda/oandapy
 export PYTHONPATH
-python robotrunner.py --account 704552 --settings settings05.json
+python robotrunner.py --account 704552 --settings settings05.json #@
 
