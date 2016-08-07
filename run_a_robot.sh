74ba97fa513d06c131a4cc4604050d6e99698d4f
DennisFantoni <dennis@fantoni.dk>
2016-08-07 21:50:37 +0200

    use echo instead of title

--- run_a_robot.sh
+++ run_a_robot.sh
@@ -3,6 +3,7 @@ PYTHONPATH=${PYTHONPATH}:/home/ubuntu01/IdeaProjects/QuantoniOanda/oanda/oandapy
 export PYTHONPATH
 for ((;;))
 do
+    echo -en "\033]0;${2}__${3}\a"
     echo robotrunner.py --account $1 --settings settings_$2.json --MCP_buytrigger $3 --MCP_selltrigger $4 $5 $6
     python robotrunner.py --account $1 --settings settings_$2.json --MCP_buytrigger $3 --MCP_selltrigger $4 $5 $6
     sleep 2s
